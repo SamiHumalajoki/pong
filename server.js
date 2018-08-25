b46d7ce0db454ba2e87c3dff731d049f6c128b62
SamiHumalajoki <42648047+SamiHumalajoki@users.noreply.github.com>
2018-08-25 20:13:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,50 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
-
+/*
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+*/
+/////////////////////////////////
+
+var leftId;
+var rightId;
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+	socket.join('room1');
+	if (leftId==null) {
+		leftId = socket.id;
+    	console.log('left player: ' + leftId);
+    	socket.emit('side', 'left');
+    }
+    else {
+    	rightId = socket.id;
+    	console.log('right player: ' + rightId);
+    	socket.emit('side', 'right');
+    	data = {
+    		x: 500,
+    		y: 500,
+    		vy: 2,
+    		vx: 10
+    	};
+    	//io.sockets.emit('start', data);
+    	io.to('room1').emit('start', data);
+    }
+
+    socket.on('disconnect', function(){
+    	console.log('user ' + socket.id + ' disconnected');
+    });
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        //console.log(data);
+    }
+}
