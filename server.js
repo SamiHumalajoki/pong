8280429e44ecd61c833ed90d54c3e4f2fcff1340
SamiHumalajoki <42648047+SamiHumalajoki@users.noreply.github.com>
2018-08-25 10:38:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,21 @@
-var express = require('express');
+'use strict';
 
-var app = express();
-var server = app.listen(3000);
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-console.log("My socket server is running")
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-var socket = require('socket.io');
+const io = socketIO(server);
 
-var io = socket(server);
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
 
-var leftId;
-var rightId;
-
-io.sockets.on('connection', newConnection);
-
-function newConnection(socket) {
-	socket.join('room1');
-	if (leftId==null) {
-		leftId = socket.id;
-    	console.log('left player: ' + leftId);
-    	socket.emit('side', 'left');
-    }
-    else {
-    	rightId = socket.id;
-    	console.log('right player: ' + rightId);
-    	socket.emit('side', 'right');
-    	data = {
-    		x: 500,
-    		y: 500,
-    		vy: 2,
-    		vx: 10
-    	};
-    	//io.sockets.emit('start', data);
-    	io.to('room1').emit('start', data);
-    }
-
-    socket.on('disconnect', function(){
-    	console.log('user ' + socket.id + ' disconnected');
-    });
-
-    socket.on('mouse', mouseMsg);
-
-    function mouseMsg(data) {
-        socket.broadcast.emit('mouse', data);
-        //console.log(data);
-    }
-}
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
